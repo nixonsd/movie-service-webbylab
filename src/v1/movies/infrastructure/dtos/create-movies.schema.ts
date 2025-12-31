b0b3417635cb4cecab77c9badb4db363e07d304b
Bohdan Kiselov <bandanovvs@gmail.com>
2025-12-31 02:52:27 +0200

    Minor DTOs changes

--- src/v1/movies/infrastructure/dtos/create-movies.schema.ts
+++ src/v1/movies/infrastructure/dtos/create-movies.schema.ts
@@ -1,8 +1,19 @@
 import { z } from 'zod';
 
+const actorNameRegex = /^[A-Za-z.,\- ]+$/;
+
 export const CreateMovieSchema = z.object({
-  title: z.string().min(1).max(255),
+  title: z.string().trim().min(1).max(255),
   year: z.number().int().min(1888).max(new Date().getFullYear()),
   format: z.enum(['VHS', 'DVD', 'Blu-Ray']),
-  actors: z.array(z.string().min(1).max(100)).min(1),
+  actors: z
+    .array(
+      z
+        .string()
+        .trim()
+        .min(3)
+        .max(100)
+        .regex(actorNameRegex, 'Actor name may contain only letters, spaces, and characters: - , .')
+    )
+    .min(1),
 });
--- src/v1/users/infrastructure/dtos/create-user.schema.ts
+++ src/v1/users/infrastructure/dtos/create-user.schema.ts
@@ -2,10 +2,10 @@ import z from 'zod';
 
 export const CreateUserSchema = z
   .object({
-    name: z.string().min(3).max(30),
+    name: z.string().trim().min(3).max(30),
     email: z.email(),
-    password: z.string().min(6).max(100),
-    confirmPassword: z.string().min(6).max(100),
+    password: z.string().trim().min(6).max(100),
+    confirmPassword: z.string().trim().min(6).max(100),
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: 'Passwords do not match',
